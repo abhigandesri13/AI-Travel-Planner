456f70accf2db1a2c6acfe7f27c472b9aeb3b8c0
Gandesri Abhilash <abhigandesri@gmail.com>
2025-11-05 20:09:31 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 # Only run this file if you want to find what are the models are support by your Gemini API token
 import google.generativeai as genai
-genai.configure(api_key="AIzaSyB5ER3FZDpfZ0CDaDbyifTHCF70IM0C-Js")
+genai.configure(api_key="")
 for m in genai.list_models():
     print(m.name, " -> ", m.supported_generation_methods)
